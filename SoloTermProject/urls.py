316ebabf8f890c18cde7051826a05cf1071c8b0b
Joseph Wysocki <jwysocki@unomaha.edu>
2026-02-20 04:43:25 +0000

    Fixed errors and misnaming

--- SoloTermProject/urls.py
+++ SoloTermProject/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from calorie_tracking_app.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello_world_app.urls'))
+    path('', include('calorie_tracking_app.urls'))
 ]
--- calorie_tracking_app/urls.py
+++ calorie_tracking_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from hello_world_app import views
+from calorie_tracking_app import views
 
 urlpatterns = [
-    path('home/', views.index, name='index')
+    path('', views.index, name='index')
 ]
